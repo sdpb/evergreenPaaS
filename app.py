68263ec99d21991bc7c712311b2d50f5fa8dae38
Samuel Palacios <samuel.palacios@pyxisportal.co>
2023-03-25 06:04:41 -0500

    HWGA

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+
+import requests
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+API_URL = "http://40.118.66.81/api/"
+GET_URL = API_URL + "listar_archivos"
+PUT_URL = API_URL + "actualizar_archivo/"
 
-if __name__ == '__main__':
+@app.route("/", methods=["GET"])
+def home():
+    # Obtener la lista de archivos desde la API
+    response = requests.get(GET_URL)
+    archivos = response.json()["archivos"]
+
+    # Renderizar la plantilla de HTML
+    return render_template("index.html", archivos=archivos)
+
+@app.route("/update", methods=["POST"])
+def update():
+    # Obtener el ID del archivo seleccionado desde el formulario
+    archivo_id = request.form["archivo_id"]
+
+    # Hacer la llamada a la API de actualización de archivos
+    response = requests.put(PUT_URL + archivo_id)
+
+    # Redirigir a la página de inicio
+    return redirect(url_for('index'))
+
+if __name__ == "__main__":
     app.run()
