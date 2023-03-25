0178cdd0fc010b789be155a25d79c138da4aa876
Samuel Palacios <samuel.palacios@pyxisportal.co>
2023-03-25 05:06:29 -0500

    Mejoras

--- static/script.js
+++ static/script.js
@@ -1,35 +1,49 @@
-const formularioActualizar = document.getElementById('formulario-actualizar');
+const archivoLista = document.getElementById("archivo-lista");
 
-formularioActualizar.addEventListener('submit', (event) => {
-  event.preventDefault();
+// Consumir la API para obtener la lista de archivos
+fetch(`${process.env.API_URL}/api/listar_archivos`)
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(archivo => {
+      const li = document.createElement("li");
+      li.innerText = `${archivo.id} - ${archivo.nombre}`;
+      li.addEventListener("click", () => seleccionarArchivo(archivo.id));
+      archivoLista.appendChild(li);
+    });
+  })
+  .catch(error => console.error(error));
 
-  const archivoId = document.getElementById('archivo-id').value;
-  const archivoNuevoNombre = document.getElementById('archivo-nuevo-nombre').value;
+function seleccionarArchivo(id) {
+  const archivoIdInput = document.createElement("input");
+  archivoIdInput.type = "hidden";
+  archivoIdInput.id = "archivo-id";
+  archivoIdInput.value = id;
+  document.body.appendChild(archivoIdInput);
+}
+
+function actualizarArchivo() {
+  const archivoId = document.getElementById("archivo-id").value;
+  const archivoNuevoNombre = document.getElementById("archivo-nuevo-nombre").value;
 
   // Validar que se ingresó un ID y un nuevo nombre
   if (!archivoId || !archivoNuevoNombre) {
-    alert('Por favor ingresa el ID del archivo y el nuevo nombre.');
+    alert("Por favor ingresa el ID del archivo y el nuevo nombre.");
     return;
   }
 
-  // Construir la solicitud POST
-  const data = new FormData();
-  data.append('archivo_id', archivoId);
-  data.append('archivo_nuevo_nombre', archivoNuevoNombre);
+  // Construir la solicitud PUT
+  const requestOptions = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ nombre: archivoNuevoNombre })
+  };
 
-  fetch('/actualizar_archivo', {
-    method: 'POST',
-    body: data
-  })
-  .then(response => response.json())
-  .then(data => {
-    if (data.error) {
-      alert(data.error);
-    } else {
-      alert(data.mensaje);
-    }
-  })
-  .catch(error => {
-    alert('Hubo un error al actualizar el nombre del archivo.');
-  });
-});
+  // Consumir la API para actualizar el nombre del archivo
+  fetch(`${process.env.API_URL}/api/actualizar_archivo/${archivoId}`, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      alert(`El nombre del archivo con ID ${data.id} fue actualizado exitosamente a ${data.nombre}.`);
+    })
+    .catch(error => console.error(error));
+}
+
